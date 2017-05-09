8294d9e10ccadab80de053f344ea2249b9332b0e
Alexandros Kalavitis <alexkalavitis@gmail.com>
2017-05-09 15:36:33 +0300

    Final

--- srt-fixer.py
+++ srt-fixer.py
@@ -16,45 +16,56 @@ with open(args.fname,newline='') as ifp:
 	
 		# -- αντικαταστήστε με τον δικό σας κώδικα (αρχή) --
 
+		# Time calcs based on argument offset
 		h = int(float(args.offset)//3600)
 		mi = int(float(args.offset)%3600//60)
 		sec = int(float(args.offset%60))
-
 		a = str(args.offset).split('.')
 		mil = int(a[1])
 
+		# Regular expr for time
 		rexp = re.compile('\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}')
 		found = rexp.search(line)
+		# If patern found do
 		if found:
+			# flush stdout
 			sys.stdout.flush()
+			# remove unwanted characters
 			temp = re.sub('--> ','', line)
 			temp = re.sub('\r\n','', temp)
-			temp = temp.split(' ')
 
+			# Split to [H,S,M,MIL]
+			temp = temp.split(' ')
 			temp[0] = temp[0].split(':')
 			temp[0][2] = temp[0][2].split(',')
-
 			temp[1] = temp[1].split(':')
 			temp[1][2] = temp[1][2].split(',')
 
+			# Add offset
+			# Start
 			temp[0][0] = str(int(temp[0][0]) + h)
 			temp[0][1] = str(int(temp[0][1]) + mi)
 			temp[0][2][0] = str(int(temp[0][2][0]) + sec)
 			temp[0][2][1] = str(int(temp[0][2][1]) + mil)
-
+			# End
 			temp[1][0] = str(int(temp[0][0]) + h)
 			temp[1][1] = str(int(temp[0][1]) + mi)
 			temp[1][2][0] = str(int(temp[1][2][0]) + sec)
 			temp[1][2][1] = str(int(temp[1][2][1]) + mil)
 
-			# print(line)
+			# Make current line new line
 			line = '\n'
+
+			# Set format Patern
 			timeStampFormation = "{}:{}:{},{} --> {}:{}:{},{}"
-			#a = str(line[0][0])
+
+			# Sent result to stdout without new line at the end of it
 			print(timeStampFormation.format(temp[0][0],temp[0][1],temp[0][2][0],temp[0][2][1],temp[1][0],temp[1][1],temp[1][2][0],temp[1][2][1]), end='')
 
+		# Write stdout to line
 		sys.stdout.write(line)
+		# flush stdout
 		sys.stdout.flush()
-
+		
 		# -- αντικαταστήστε με τον δικό σας κώδικα (τέλος) --
 
